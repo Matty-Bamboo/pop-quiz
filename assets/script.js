38ed6f05e1e825fc21b42faba26fd26506d186ad
Matt Miceli <126421060+Matty-Bamboo@users.noreply.github.com>
2023-04-08 15:31:26 -0700

    update css and html files

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+//user clicks Start Quiz Button
+  // then timer starts 
+  // User is presented with first question
+    //if correct alert: Correct!
+    //10 points added to score
+    //if incorrect alert: Wrong!
+    //10 seconds subtracted from time
+  // User is presented with second question...
+  // When all 5 questions are answered OR the timer reaches zero
+    //then game is over
+ //User can then enter & save intials and score.
+ //optional user can "clear" scores
+
+ // an array with 5 questions
+ 
