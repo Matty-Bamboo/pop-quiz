a275d1c804a3314c54c7801504b8e12d2761b613
Matt Miceli <126421060+Matty-Bamboo@users.noreply.github.com>
2023-04-09 12:53:55 -0700

    update javascript

--- assets/script.js
+++ assets/script.js
@@ -11,5 +11,38 @@
  //User can then enter & save intials and score.
  //optional user can "clear" scores
 
- // an array with 5 questions
- 
+
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+var score;
+var initials;
+var answer;
+var isWin = false;
+var timer;
+var timerCount;
+
+
+// The init function is called when the page loads 
+function init() {
+  getscore();
+  getinitals;
+}
+
+// array of questions/answers
+var questions = [
+  {"question" : "What does HTML stand for?" , "options" : ["cascading style sheet","hypertext Markup Language","Hypocratic Marker Language","Non of the above"], "answer" : "hypertext Markup Language"},
+  {"question" : "String values must be enclosed within _____ when being assinged to variables" , "options" : ["commas","curly brackets","paranthasis","quotes"], "answer" : "quotes"},
+  {"question" : "Arrays in JavaScript can be used to store" , "options" : ["numbers and strings","other arrays","booleans","all of the above"], "answer" : "all of the above"},
+  {"question" : "which is NOT a data type?" , "options" : ["strings","booleans","numbers","alerts"], "answer" : "alerts"},
+  {"question" : "Which of the following methods is used to access HTML elements using Javascript" , "options" : ["getElementbyId()","getElementsByClassName()","Both 1 & 2","Non of the above"], "answer" : "Both 1 & 2"},
+];
+
+// The startGame function is called when the start button is clicked
+function startquiz() {
+  isWin = false;
+  timerCount = 90;
+// Prevents start button from being clicked when round is in progress
+  startButton.disabled = true;
+  startTimer()
+}
