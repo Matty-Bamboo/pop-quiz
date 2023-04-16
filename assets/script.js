5d5a9253f7bb6b2984942abb37e40d97131ab35e
Matt Miceli <126421060+Matty-Bamboo@users.noreply.github.com>
2023-04-16 14:06:04 -0700

    updated javaSript, CSS & html

--- assets/script.js
+++ assets/script.js
@@ -11,153 +11,190 @@
  //User can then enter & save intials and score.
  //optional user can "clear" scores
 
-// elements by ID
+ // Define the quiz questions
 
-var ul = document.getElementById('ul')
-var nextButton = document.getElementById('btnNext');
-var quizbox = document.getElementById('questionBox')
-var opt1 = document.getElementById('opt1')
-var opt2 = document.getElementById('opt2')
-var opt3 = document.getElementById('opt3')
-var opt4 = document.getElementById('opt4')
-var startButton = document.querySelector("#start-button");
-var timerElement = document.querySelector(".timer-count");
-var restartButton = document.querySelector("#reset-button");
-var timer;
-var timerCount;
-var isWin = false;
+ let currentQuestionIndex = 0;
+ let score = 0;
 
-// array of questions/answers
-var app = {
-  questions: [
-  {
-    question: "What does HTML stand for?",
-     options: ["cascading style sheet", "hypertext markup language", "hypocratic marker language", "none of the above"],
-     answer: "2"
-  },
-  {
-    question: "String values must be enclosed within _____ when being assinged to variables",
-     options: ["commas", "curly brackets", "paranthasis", "quotes"],
-     answer: "4"
-  },
-  {
-    question: "Arrays in JavaScript can be used to store",
-      options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-      answer: "4"
-  },
-  {
-    question: "which is NOT a data type?",
-      options: ["strings", "booleans", "numbers", "alerts"],
-      answer: "4"
-  },
-  {
-    question: "Which of the following methods is used to access HTML elements using Javascript",
-      options: ["getElementbyId()", "getElementsByClassName()", "both 1 & 2", "none of the above"],
-      answer: "3"
-  },
-],
-
-index:0,
- //Load a question using the index
- load:function(){
-  if(this.index<=this.questions.length-1){
-    quizbox.innerHTML=this.index+1 + ". " +this.questions[this.index].question;
-    opt1.innerHTML=this.questions[this.index].options[0];
-    opt2.innerHTML=this.questions[this.index].options[1];
-    opt3.innerHTML=this.questions[this.index].options[2];
-    opt4.innerHTML=this.questions[this.index].options[3];
-  }
-   else {
- //Show the end screen
-    quizbox.innerHTML="Quiz Completed!";
-    ul.style.display="none";
-    nextButton.style.display="none";
-  }
+ const quizQuestions = [
+ {
+ question: "What does HTML stand for?",
+ options: ["cascading style sheet", "hypertext markup language", "hypocratic marker language", "none of the above"],
+ answer: "hypertext markup language"
  },
- next: function(){
-  this.index++;
-  this.load();
+ {
+ question: "String values must be enclosed within _____ when being assinged to variables",
+ options: ["commas", "curly brackets", "paranthasis", "quotes"],
+ answer: "quotes"
  },
-//Check if answer is correct or not
- check: function(ele){
-  var id=ele.id.split('');
-  if(id[id.length-1]==this.questions[this.index].answer){
-    this.score++;
-    ele.className="correct";
-    this.scoreCard();
-  }
-  else{
-   ele.className="wrong";
-  }
-},
-//disable options once user selects on option
-preventClick:function(){
-  for(let i=0; i<ul.children.length; i++){
-  ul.children[i].style.pointerEvents="none";
-  }
-},
-allowClick:function(){
-  for(let i=0; i<ul.children.length; i++){
-    ul.children[i].style.pointerEvents="auto";
-    ul.children[i].className=''
-  }
-},
-  score:0,
-  scoreCard:function(){
-  scoreCard.innerHTML=this.questions.length + "/" + this.score;
+ {
+ question: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+ options: ["last()", "put()", "push()", "none of the above"],
+ answer: "push()"
+ },
+ {
+ question: "Arrays in JavaScript can be used to store;",
+ options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+ answer: "all of the above"
+ },
+ {
+ question: "Which of the following is not a JavaScript data type?",
+ options: ["undefined", "number", "string", "float"],
+ answer: "float"
+ }
+ ];
+  
+// Define a function to update the timer element and change its color to red when time is running out
+function updateTimer() {
+// Get the timer element
+ const timerEl = document.getElementById("timer");
+// Get the remaining time in seconds
+ let timeLeft = parseInt(timerEl.textContent);
+// Decrement the time left by 1 second
+ timeLeft--;
+// Update the timer element with the new time left
+ timerEl.textContent = timeLeft;
+ // If time left is less than 20 seconds, change the color of the timer element to red
+ if (timeLeft < 20) {
+  timerEl.style.color = "red";
+ }
+
+ // If time is up, end the quiz
+
+ if (timeLeft <= 0) {
+  clearInterval(timerInterval);
+  timerEl.textContent = "TIME'S UP!";
+  document.getElementById("questionBox").textContent = "Time's up!";
+  document.querySelector(".quiz-container").classList.replace("quiz-container", "quiz-container-hidden");
+  document.querySelector(".endQuiz-hidden").classList.replace("endQuiz-hidden", "endQuiz");
+ }
 }
-}
+  
+// Start the timer checker
 
-window.load=app.load();
+let timerInterval = setInterval(updateTimer, 1000);
+  
+// Set the timer and score variables
+let timeLeft = 60;
+  
+// Define the functions for starting, displaying, and progressing through the quiz
 
-function button(ele){
-    app.check(ele);
-    app.preventClick();
-}
-
-function next(){
-    app.next();
-    app.allowClick();
-}
-
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", function() {
-  document.querySelector("#main-quiz") .setAttribute("class", "show");
-  document.querySelector("#start-page") .setAttribute("class", "hide");
-});
-
-function resetQuiz() {
-  scoreCounter = 0;
-  setScore();
-}
-
-restartButton.addEventListener("click", restartQuiz);
-
-// The startQuiz function is called when the start button is clicked
 function startQuiz() {
-  isWin = false;
-  timerCount = 10;
-  // Prevents start button from being clicked when round is in progress
-  //startButton.disabled = true;
-  startTimer()
+  localStorage.clear()
+  document.getElementById("start-button").classList.add("hide");
+  document.getElementById("rules").style.display = "none";
+  document.querySelector('.quiz-container-hidden').classList.replace('quiz-container-hidden', 'quiz-container');
+
+  // Change its class to "quiz-container"
+  
+  next();
+  setTimer();
+}
+  
+function displayQuestion(currentQuestionIndex) {
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+    document.getElementById("questionBox").textContent = currentQuestion.question;
+    for (let i = 0; i < currentQuestion.options.length; i++) {
+      document.getElementById("opt" + (i + 1)).textContent = currentQuestion.options[i];
+    }
+    document.getElementById("btnNext").classList.add("hide");
+}
+  
+function next() {
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+ if (currentQuestionIndex === quizQuestions.length-1) {
+  document.querySelector(".quiz-container").classList.add("quiz-container-hidden");
+  document.querySelector(".endquiz-hidden").classList.add("endQuiz");
+  document.querySelector(".endQuiz-hidden").classList.remove("endQuiz-hidden");
+
+  localStorage.setItem("score", score);
+ }
+ else {
+  document.getElementById("options").classList.remove("no-click");
+  document.getElementById("quiz-result").classList.add("hide");
+
+  displayQuestion(currentQuestionIndex);
+
+  currentQuestionIndex++;
+ }
+}
+
+function evaluateSubmission() {
+  const selectedOption = document.querySelector("selected");
+  // minus one from current question index since it was incrementd by next function
+  const currentQuestion = quizQuestions[currentQuestionIndex-1];
+
+  if (selectedOption) {
+    const answer = selectedOption.textContent;
+    document.getElementById("quiz-result").classList.remove("hide");
+    if (answer === currentQuestion.answer) {
+      score++;
+      const quizResult = document.getElementById("quiz-result");
+      quizResult.textContent = "Correct!";
+
+      document.getElementById("quiz-result").classList.add("theme-success");
+      }
+    else {
+      timeLeft -= 10;
+      const quizResult = document.getElementById("quiz-result");
+      quizResult.textContent = "Incorrect!";
+
+      document.getElementById("quiz-result").classList.add("theme-failure");
+    }
+
+    document.getElementById("btnNext").classList.remove("hide");
+    document.getElementById("btn-submit").classList.add("hide");
+    document.getElementById("options").classList.add("no-click");
+  }
+}
+  
+function displayFinalScore() {
+  let score = localStorage.getItem("score");
+  const intials = document.getElementById("initials").value;
+  document.querySelector(".endQuiz").classList.replace("endQuiz", "hide");
+  document.querySelector("final-hidden").classList.replace("final-hidden", "endQuiz");
+
+  const finalScoreSpan = document.getElementById("finalScore");
+  finalScoreSpan.textContent = score*10;
+
+  const userInitials = document.getElementById("userInitials");
+  userInitials.textContent = initials;
+}
+
+function setTimer() {
+ document.getElementById("timer").textContent = timeLeft;  
+ timeLeft--;
+ timerInterval = setInterval(function() {
+  document.getElementById("timer").textContent = timeLeft;
+ if (timeLeft <= 0) {
+  document.getElementById("btnNext").disabled = true;
+  }, 1000);
+}
 }
 
 
-timer = setInterval(function() {
-  timerCount--;
-  timerElement.textContent = timerCount;
-  if (timerCount >= 0) {
-    // Tests if win condition is met
-    if (isWin && timerCount > 0) {
-      // Clears interval and stops timer
-      clearInterval(timer);
-      winGame();
-    }
+// add event listenes to the ansswer options and start button
+const answerOptions = document.querySelectorAll(".options li");
+for (let i = 0; i < answerOptions.length; i++) {
+  answerOptions[i].addEventListener("click", function() {
+    const selected = document.querySelector("selected");
+    if (selected) selected.classList.remove("selected");
+    this.classList.add("selected");
+    document.getElementById("btn-submit").classList.remove("hide");
+  });
+}
+
+function button(elem) {
+  const previouslySelected = document.querySelector(".selected");
+  if (previouslySelected) {
+    previouslySelected.classList.remove("selected");
   }
-  // Tests if time has run out
-  if (timerCount === 0) {
-    // Clears interval
-    clearInterval(timer);
-    loseGame();
-  }
-}, 1000);
+  elem.classList.add("selected");
+
+  const nextButton = document.getElementById("btnNext");
+  nextButton.addEventListener("click", () => {
+    elem.classList.remove("selected");
+  });
+}
+
+document.getElementById("start-button").addEventListener("click", startQuiz);
